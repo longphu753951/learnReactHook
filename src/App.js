4eb82e3ccf7615bb0e9cf66bc8355c1b33a75979
Long Phu <phutl@icd-vn.com>
2022-02-22 11:14:22 +0700

    update learn use useref

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  const [name, setName] = useState('');
+  const renderCount = useRef(0);
+  const inputRef = useRef();
+  const prevName = useRef('');
+
+  const focus = () => {
+    inputRef.current.focus();
+  }
+
+  useEffect(() => {
+    renderCount.current += 1;
+  })
+
+  useEffect(() => {
+    prevName.current = name;
+  }, [name])
+
+  return(
+    <>
+      <input ref ={inputRef} value = {name} onChange = { e => setName(e.target.value)} />
+      <div>My name is {name} and it used to be {prevName.current}</div>
+      <div>Render count: {renderCount.current}</div>
+      <button onClick={focus}>Focus</button>
+    </>
+  )
 }
 
 export default App;
