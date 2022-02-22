80bb04bfd8f68186badb3510230a8278d562a6c8
Long Phu <phutl@icd-vn.com>
2022-02-22 10:45:36 +0700

    update learn use effect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [resourceType, setResourceType] = useState("posts");
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [items, setItems] = useState([]);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  //use as componentDidMount
+  useEffect(() => {
+    console.log("component did mount");
+    window.addEventListener("resize", handleResize);
+  }, []);
+
+  //use as componentDidUpdate for state resourceType
+  useEffect(() => {
+    console.log("component did update");
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+
+    //use as componentWillUnmount or clean up pre code
+    return () => {
+      console.log("clean up old item");
+      setItems([]);
+    };
+  }, [resourceType]);
+
+  // //use as componentDidUpdate for state items
+  // useEffect(() => {
+  //   console.log('items has change');
+  // }, [items]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div>{windowWidth}</div>
+      <div>
+        <button
+          onClick={() => {
+            setResourceType("posts");
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Post
+        </button>
+        <button
+          onClick={() => {
+            setResourceType("users");
+          }}
+        >
+          Users
+        </button>
+        <button
+          onClick={() => {
+            setResourceType("comments");
+          }}
+        >
+          Comments
+        </button>
+      </div>
+      <h1>{resourceType}</h1>
+      <div>
+        {items.map((item) => {
+          return <pre key={item.id}>{JSON.stringify(item)}</pre>;
+        })}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
